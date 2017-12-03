ac08a5e45ffb2ddf377555822058fea8585a735e
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-12-03 20:17:35 +0530

    Adding first react component

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,12 @@
+'use strict';
+
 console.log('app.js is running');
+
+var template = React.createElement(
+  'h1',
+  { id: 'someid' },
+  'This is JSX expression'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
